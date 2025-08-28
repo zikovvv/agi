bc9d913aba6adddc7250fed120be6841488885a7
zikovvv <djmisha109735@gmail.com>
2025-08-29 02:14:46 +0400

    Add evaluation plotting functionality and update training configuration

--- encoder/masked_sequence_modeling_few_puzzles/train.py
+++ encoder/masked_sequence_modeling_few_puzzles/train.py
@@ -17,6 +17,9 @@ from common import *
 from datasets import Dataset
 import torch
 from torch import nn
+import matplotlib.pyplot as plt
+
+
 
 class SimpleARCDataset(Dataset):
     def __init__(self,
@@ -43,6 +46,7 @@ class DatasetConfig:
 
 @dataclass
 class TrainConfig:
+    nb_plots_on_val : int = 2
     epochs: int = 50
     batch_size: int = 16
     lr: float = 3e-4
@@ -75,13 +79,12 @@ def train_one_epoch(model: nn.Module,
         
         preds = logits.argmax(dim=-1)
         nb_corr_cur = (preds == labels).sum().item()
-        nb_masked = (labels == ignored_id).sum().item()
+        nb_masked = (labels != ignored_id).sum().item()
         total_correct += nb_corr_cur
         total_classified += nb_masked
         
         total_loss += loss.item() * input_ids.size(0)
-
-        total += 1
+        total += input_ids.shape[0]
 
 
 
@@ -89,12 +92,85 @@ def train_one_epoch(model: nn.Module,
     acc = total_correct / total_classified
     return {"loss": avg_loss, "acc": acc}
 
-@torch.no_grad()
-def evaluate(model: nn.Module,
-             loader: DataLoader,
-             device: str = "cpu") -> Dict[str, float]:
-    ignored_id  : int = model.cfg.ignore_id # type: ignore
 
+
+
+def plot_eval_batch(
+    input_ids : torch.Tensor,
+    rows : torch.Tensor,
+    cols : torch.Tensor,
+    types : torch.Tensor,
+    labels : torch.Tensor,
+    labels_predicted : torch.Tensor
+) -> None:
+    B, L = input_ids.shape
+    MAX_COLOR = 30
+    for i in range(B):
+        max_w, max_h = int(cols[i].max().item() + 1), int(rows[i].max().item() + 1) 
+        q_grid = np.full((max_h, max_w), MAX_COLOR)
+        a_grid = np.full((max_h, max_w), MAX_COLOR)
+        l_grid = np.full((max_h, max_w), MAX_COLOR)
+        cols_grid = np.full((max_h, max_w), MAX_COLOR)
+        rows_grid = np.full((max_h, max_w), MAX_COLOR)
+        types_grid = np.full((max_h, max_w), MAX_COLOR)
+        labels_pred_grid = np.full((max_h, max_w), MAX_COLOR)
+        labels_pred_grid_over_a_input = np.full((max_h, max_w), MAX_COLOR)
+        # assert labels has somtehing except -100
+        assert (labels != -100).sum() > 0
+        for inp, r, c, t, l, lp in zip(input_ids[i], rows[i], cols[i], types[i], labels[i], labels_predicted[i]):
+            if r != -100  and c != -100 :
+                if t == 0 :
+                    q_grid[r, c] = inp
+                else :
+                    a_grid[r, c] = inp
+                    if l != -100 :
+                        l_grid[r, c] = l
+                        labels_pred_grid[r, c] = lp
+                        labels_pred_grid_over_a_input[r, c] = lp
+                    else :
+                        labels_pred_grid_over_a_input[r, c] = inp
+
+                cols_grid[r, c] = c
+                rows_grid[r, c] = r
+                types_grid[r, c] = t
+
+        fig, axs = plt.subplots(4, 2, figsize=(6, 12))
+
+        axs[0, 0].imshow(q_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[0, 0].set_title(f'IN Questions')
+
+        axs[1, 0].imshow(a_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[1, 0].set_title(f'IN Answers')
+
+        axs[2, 0].imshow(l_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[2, 0].set_title(f'IN Labels')
+
+        axs[0, 1].imshow(types_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[0, 1].set_title(f'IN Types')
+
+        axs[1, 1].imshow(rows_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[1, 1].set_title(f'IN Rows')
+
+        axs[2, 1].imshow(cols_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[2, 1].set_title(f'IN Cols')
+
+        axs[3, 0].imshow(labels_pred_grid, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[3, 0].set_title(f'Predicted Labels')
+
+        axs[3, 1].imshow(labels_pred_grid_over_a_input, cmap='tab20', vmin=0, vmax=MAX_COLOR)
+        axs[3, 1].set_title(f'Predicted Labels over Input Answers')
+
+        plt.show()
+
+
+@torch.no_grad()
+def evaluate(
+    model: nn.Module,
+    loader: DataLoader,
+    nb_plots_on_val: int = 2
+) -> Dict[str, float]:
+    ignored_id  : int = model.cfg.ignore_id # type: ignore
+    plotted_examples = 0
     model.eval()
     total = 0
     total_loss = 0.0
@@ -105,15 +181,18 @@ def evaluate(model: nn.Module,
         res = model(input_ids, rows, cols, token_types_ids, labels)
         loss = res['loss']
         logits = res['logits']
-        preds = logits.item().argmax(dim=-1)
+        preds = logits.argmax(dim=-1)
         nb_corr_cur = (preds == labels).sum().item()
-        nb_masked = (labels == ignored_id).sum().item()
+        nb_masked = (labels != ignored_id).sum().item()
         total_correct += nb_corr_cur
         total_classified += nb_masked
         
         total_loss += loss.item() * input_ids.size(0)
 
-        total += 1
+        total += input_ids.shape[0]
+        if plotted_examples < nb_plots_on_val:
+            plot_eval_batch(input_ids, rows, cols, token_types_ids, labels, preds)
+            plotted_examples += 1
 
     avg_loss = total_loss / max(total, 1)
     acc = total_correct / total_classified
@@ -135,7 +214,7 @@ def main(
     tcfg: TrainConfig,
     mcfg: ARCEncoderConfig,
 ):
-    tcfg.device = 'cuda'
+    tcfg.device = 'cpu'
     # device = tcfg.device
     model = _get_model(mcfg, tcfg)
     optimizer = _get_optimizer(model, tcfg)
@@ -173,7 +252,7 @@ def main(
         
         l  = f"Epoch {epoch:02d} | "
         with torch.no_grad():
-            va = evaluate(model, val_dl, device=tcfg.device)
+            va = evaluate(model, val_dl, nb_plots_on_val = tcfg.nb_plots_on_val)
         for k, v in va.items() : 
             l += f"{k} {v:.4f} | "
         print(f"{l}")
@@ -194,7 +273,7 @@ if __name__ == "__main__":
         val_frac=0.1,
         test_frac=0.0,
         percentage_masked=0.2,
-        num_samples=1000
+        num_samples=6
     )
     _tcfg = TrainConfig(
         batch_size=2,
